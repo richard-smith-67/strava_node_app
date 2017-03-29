0bd02f6372b6dffa635c5d9bce2e21949b0bb4ef
richard-smith-67 <richard.smith.67@gmail.com>
2017-03-29 16:25:19 +0100

    added xhr

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ app.get('/', function (req, res) {
     //res.send("Home page");
     // res.redirect('https://www.strava.com/oauth/authorize?client_id=9&response_type=code&redirect_uri=http://testapp.com/token_exchange&scope=write&state=mystate&approval_prompt=force')
 
-     res.redirect('https://www.strava.com/oauth/authorize?client_id=14088&response_type=code&redirect_uri=http://localhost:3000/access.html&scope=write&state=mystate&approval_prompt=force')
+    var client_id = 'xxxx'
+
+     res.redirect('https://www.strava.com/oauth/authorize?client_id=' + client_id + '&response_type=code&redirect_uri=http://localhost:3000/access.html&scope=write&state=mystate&approval_prompt=force')
    
 });
 
 app.get('/athlete', function (req, res) {
  
-  strava.athlete.get({id:14088},function(err,payload) {
+  strava.athlete.get({id:xxxx},function(err,payload) {
     if(!err) {
         res.send(payload)
     }
